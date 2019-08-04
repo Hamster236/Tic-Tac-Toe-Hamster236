fc756e26db2a5dc2c421bd28446b9fc1eb883ff5
Hamster236 <esub92@gmail.com>
2019-08-04 14:49:37 -0600

    Game Board code cleanup

--- DrawBoard.py
+++ DrawBoard.py
@@ -3,18 +3,42 @@ import GameFunctions as Gf
 
 class board:
     def __init__(self, window, boardSize):
+        print("[INFO] Generating Game Board...")
+
+        # Creating class variables
         self.window = window
         self.boardSize = boardSize
+
+        # Defining the expandable button array
         self.button = []
+
+        # Setting the window size
+        # TODO: Make expandable based on game size.
         self.window.geometry('265x350')
         self.window.config(bg='White')
         self.window.title("Tic-Tac-Toe")
+
+        # Configuring Game Board
         self.__buttons__()
         self.__label__()
+
+        # Importing the game functionality
         self.Gf = Gf.gameFunctions(self.window, self.gameInfo, self.boardSize)
+        print("[INFO] Board Generated!")
+
+        # Starting the GUI
         self.window.mainloop()
 
     def __label__(self):
+        '''
+        Game Labels
+        '''
+        # In window title
+        self.gameIntro = tk.Label(self.window, text="Hamster's Tic-Tac-Toe", font=('ariel',14),
+                                  width=28, anchor=tk.NW)
+        self.gameIntro.grid(row=0, column=0, columnspan=35, padx=(5,0), pady=(5,0))
+
+        # Game update message block
         self.gameInfo = tk.Label(self.window, text="", font=('ariel',14), width=28, anchor=tk.NW)
         self.gameInfo.grid(row=5, column=0, columnspan=35, padx=(5,0), pady=(5,0))
 
