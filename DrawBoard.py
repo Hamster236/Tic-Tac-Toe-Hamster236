adbe47c91c719fcd2de9d7ff180f7bbe83f6fcc6
Hamster236 <esub92@gmail.com>
2019-08-09 17:17:50 -0600

    Added improved Title screen

--- DrawBoard.py
+++ DrawBoard.py
@@ -2,13 +2,34 @@ import tkinter as tk
 import GameFunctions as Gf
 
 class Board:
-    def __init__(self, window, boardSize):
-        print("[INFO]\tGenerating Game Board...")
-
+    def __init__(self, window):
         # Creating class variables
         self.window = window
-        self.boardSize = boardSize
 
+    def __label__(self):
+        '''
+        Game Labels
+        '''
+        # In window title
+        self.gameIntro = tk.Label(self.frameBoard, text="Hamster's Tic-Tac-Toe", font=('ariel',14),
+                                  width=28, anchor=tk.NW)
+        self.gameIntro.grid(row=0, column=0, columnspan=35, padx=(5,0), pady=(5,0))
+
+        # Game update message block
+        self.gameInfo = tk.Label(self.frameBoard, text="", font=('ariel',14), width=28, anchor=tk.NW)
+        self.gameInfo.grid(row=5, column=0, columnspan=35, padx=(5,0), pady=(5,0))
+
+    def __buttons__(self, boardSize):
+        '''
+        Dynamic button generation.
+        '''
+        
+        for i in range(0, boardSize * boardSize):
+            self.button.append(tk.Button(self.frameBoard,text='-',font=('ariel',14),bg='White',width=6,height=3,
+                                         command=lambda i=i:self.Gf.playerSelect(self.button[i], i+1)))
+            self.button[i].grid(row=(int(i/boardSize)%boardSize)+1,column=(int(i)%boardSize))
+
+    def __create__(self, boardSize):
         # Creating the frame for the board. This will be displayed under the
         # title frame
         self.frameBoard = tk.Frame(self.window, bg='White')
@@ -17,42 +38,9 @@ class Board:
         # Defining the expandable button array
         self.button = []
 
-        # Setting the window size
-        # TODO: Make expandable based on game size.
-        # self.window.geometry('265x350')
-        # self.window.config(bg='White')
-        # self.window.title("Tic-Tac-Toe")
-
         # Configuring Game Board
-        self.__buttons__()
+        self.__buttons__(boardSize)
         self.__label__()
 
         # Importing the game functionality
-        self.Gf = Gf.GameFunctions(self.frameBoard, self.gameInfo, self.boardSize)
-        print("[INFO]t\tBoard Generated!")
-
-        # Starting the GUI
-        # self.window.mainloop()
-
-    def __label__(self):
-        '''
-        Game Labels
-        '''
-        # In window title
-        self.gameIntro = tk.Label(self.window, text="Hamster's Tic-Tac-Toe", font=('ariel',14),
-                                  width=28, anchor=tk.NW)
-        self.gameIntro.grid(row=0, column=0, columnspan=35, padx=(5,0), pady=(5,0))
-
-        # Game update message block
-        self.gameInfo = tk.Label(self.window, text="", font=('ariel',14), width=28, anchor=tk.NW)
-        self.gameInfo.grid(row=5, column=0, columnspan=35, padx=(5,0), pady=(5,0))
-
-    def __buttons__(self):
-        '''
-        Dynamic button generation.
-        '''
-        
-        for i in range(0, self.boardSize * self.boardSize):
-            self.button.append(tk.Button(self.window,text='-',font=('ariel',14),bg='White',width=6,height=3,
-                                         command=lambda i=i:self.Gf.playerSelect(self.button[i], i+1)))
-            self.button[i].grid(row=(int(i/self.boardSize)%self.boardSize)+1,column=(int(i)%self.boardSize))
+        self.Gf = Gf.GameFunctions(self.frameBoard, self.gameInfo, boardSize)
--- TitleFrame.py
+++ TitleFrame.py
@@ -7,12 +7,17 @@ class TitleFrame:
         # Class level Root tkinter window
         self.window = window
         
-        # Creating the board
-        db.Board(self.window, 3)
-
         # Title frame
-        self.frameTitle = tk.Frame(self.window, bg='White')
+        self.frameTitle = tk.Frame(self.window, bg='White', relief=tk.RAISED)
         self.frameTitle.grid(row=0,column=0,sticky='nsew')
+
+        self.button = []
+
+        # Creating the board
+        self.board = db.Board(self.window)
+
+        self.__labels__()
+        self.__buttons__()
 
         # Window settings
         self.window.geometry('265x350')
@@ -21,3 +26,32 @@ class TitleFrame:
 
         # Main Executer
         self.window.mainloop()
+
+    def __labels__(self):
+        mainMenu = tk.Label(self.frameTitle, text='Choose a board size', font=('ariel',20), anchor=tk.CENTER)
+        mainMenu.grid(row=1, column=1, columnspan=2)
+
+    def __buttons__(self):
+        count = 1        
+        for i in ['3x3', '5x5', '7x7', '9x9']:
+            self.button.append(tk.Button(self.frameTitle,text=str(i),font=('ariel',14),bg='White',width=10,height=2,
+                                         command=lambda count=count:self.boardSelect(count)))
+            self.button[count-1].grid(row=count+2, column=1)
+            print(count-1)
+            count = count + 1
+
+    def boardSelect(self, boardSize):
+        if boardSize is 1:
+            self.board.__create__(3)
+            self.frameTitle.destroy()
+        elif boardSize is 2:
+            self.board.__create__(5)
+            self.frameTitle.destroy()
+        elif boardSize is 3:
+            self.board.__create__(7)
+            self.frameTitle.destroy()
+        elif boardSize is 4:
+            self.board.__create__(9)
+            self.frameTitle.destroy()
+        else:
+            print("Board size not supported.")
