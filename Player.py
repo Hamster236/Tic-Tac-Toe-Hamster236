e388353f29c8f421deb50d25978f4b463473aab7
Hamster236 <esub92@gmail.com>
2019-09-29 10:17:16 -0600

    Improvement made to writing file

--- Player.py
+++ Player.py
@@ -2,4 +2,4 @@ class Player:
     name = ''
     wins = 0
     losses = 0
-    ratio = wins // losses if losses != 0 else 1
+    ratio = 0
--- SaveData.py
+++ SaveData.py
@@ -8,6 +8,7 @@ class SavedData:
     def __init__(self):
         self.filename = 'savedata.csv'
         self.info = ['Name','Wins','Losses','Ratio']
+        self.linecount = 0
         if not os.path.isfile(self.filename):
             self.writeTitle()
         
@@ -17,57 +18,56 @@ class SavedData:
             writer.writeheader()
         
     def readData(self, player1Name, player2Name):
+        #TODO: Remove the copy/paste of this code
+        #FIXME: This code can be converted to a list
         player1Name = ''.join(player1Name.split()).lower()
         player2Name = ''.join(player2Name.split()).lower()
         player1 = player.Player()
         player2 = player.Player()
         player1.name = player1Name
         player2.name = player2Name
+
+        # Saving other players for a new file.
+        self.playerlist = []
         #player2 = {'name':player2Name, 'wins':0, 'losses':0, 'ratio':0}
         with open(self.filename, 'r+') as savedata:
             reader = csv.DictReader(savedata)
-            linecount = 0
             for user in reader:
-                if linecount != 0:
+                if self.linecount != 0:
                     if user['Name'] == player1Name:
                         player1.name   = user['Name']
                         player1.wins   = int(user['Wins'])
                         player1.losses = int(user['Losses'])
                         player1.ratio  = float(user['Ratio'])
-                    if user['Name'] == player2Name:
+                        self.linecount-=1
+                    elif user['Name'] == player2Name:
                         player2.name   = user['Name']
                         player2.wins   = int(user['Wins'])
                         player2.losses = int(user['Losses'])
                         player2.ratio  = float(user['Ratio'])
-                linecount+=1
+                        self.linecount-=1
+                    else:
+                        self.playerlist.append(player.Player())
+                        self.playerlist[self.linecount-2].name = user['Name']
+                        print(self.playerlist[self.linecount-2].name)
+                self.linecount+=1
+                print(self.playerlist)
         return player1, player2
 
     def writeData(self, player1, player2):
-        writeSuccess1 = True
-        writeSuccess2 = True
-        with open(self.filename, 'a+', newline='') as savedata:
+        with open(self.filename, 'w', newline='') as savedata:
             writer = csv.DictWriter(savedata, fieldnames=self.info)
-            reader = csv.DictReader(savedata)
-            for player in reader:
-                if player['Name'] == player1.name:
-                    writer.writerow({'Name':player1.name,    \
-                                     'Wins':player1.wins,    \
-                                     'Losses':player1.losses,\
-                                     'Ratio':player1.ratio})
-                    writeSuccess1 = False
-                if player['Name'] == player2.name:
-                    writer.writerow({'Name':player2.name,    \
-                                     'Wins':player2.wins,    \
-                                     'Losses':player2.losses,\
-                                     'Ratio':player2.ratio})
-                    writeSuccess2 = False
-            if writeSuccess1:
-                writer.writerow({'Name':player1.name,    \
-                                 'Wins':player1.wins,    \
-                                 'Losses':player1.losses,\
-                                 'Ratio':player1.ratio})
-            if writeSuccess2:
-                writer.writerow({'Name':player2.name,    \
-                                 'Wins':player2.wins,    \
-                                 'Losses':player2.losses,\
-                                 'Ratio':player2.ratio})
+            writer.writeheader()
+            writer.writerow({'Name':player1.name,     \
+                             'Wins':player1.wins,     \
+                             'Losses':player1.losses, \
+                             'Ratio':player1.ratio})
+            writer.writerow({'Name':player2.name,     \
+                             'Wins':player2.wins,     \
+                             'Losses':player2.losses, \
+                             'Ratio':player2.ratio})
+            for i in range(0,self.linecount):
+                writer.writerow({'Name':self.playerlist[i].name,     \
+                                'Wins':self.playerlist[i].wins,      \
+                                'Losses':self.playerlist[i].losses,  \
+                                'Ratio':self.playerlist[i].ratio})
