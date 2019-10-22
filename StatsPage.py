ac8ff89cfbed025ef013b1f7c5d4b136fb99185b
Hamster236 <esub92@gmail.com>
2019-10-21 22:09:08 -0600

    Updated stats page to be treeview

--- StatsPage.py
+++ StatsPage.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
 class StatsPage:
     def __init__(self, window, playerlist, winner_message):
@@ -7,45 +8,72 @@ class StatsPage:
         self.winner_message = winner_message
         self.createFrame()
 
-    def createFrame(self):
-        stats_frame = tk.Frame(self.window, bg='White', relief=tk.RAISED)
-        stats_frame.grid(row=0, column=0)
 
+    def createFrame(self):
+        stats_frame = tk.Frame(self.window, relief=tk.RAISED)
+        stats_frame.grid(row=0, column=0)
         stats_canvas_winner_results = tk.Canvas(stats_frame)
         stats_canvas_winner_results.grid(row=0, column=0)
-
         self.displayWinner(stats_canvas_winner_results)
-
         stats_canvas_player_results = tk.Canvas(stats_frame)
         stats_canvas_player_results.grid(row=1, column=0)
-        
         self.displayPlayerStats(stats_canvas_player_results)
+
 
     def displayWinner(self, canvas):
         winner_message = tk.Label(canvas, text=self.winner_message)
         winner_message.grid(row=0, column=0)
 
     def displayPlayerStats(self, canvas):
-        scrollbar = tk.Scrollbar(canvas)
-        scrollbar.grid(row=1, column=5, sticky='ns')
-        self.listbox_names = tk.Listbox(canvas, width=10, yscrollcommand=scrollbar.set)
-        self.listbox_wins = tk.Listbox(canvas, width=3, yscrollcommand=scrollbar.set)
-        self.listbox_losses = tk.Listbox(canvas, width=3, yscrollcommand=scrollbar.set)
-        self.listbox_ratios = tk.Listbox(canvas, width=8, yscrollcommand=scrollbar.set)
-        for player in self.playerlist:
-            self.listbox_names.insert(tk.END, player.name)
-            self.listbox_wins.insert(tk.END, player.wins)
-            self.listbox_losses.insert(tk.END, player.losses)
-            self.listbox_ratios.insert(tk.END, player.ratio)
-        self.listbox_names.grid(row=1, column=0)
-        self.listbox_wins.grid(row=1, column=1)
-        self.listbox_losses.grid(row=1, column=2)
-        self.listbox_ratios.grid(row=1, column=3)
-        scrollbar.config(command=self.scrollbarContol)
+        tree = self.TreeviewSetup(canvas)
+        self.fillTreeview(tree)
 
 
-    def scrollbarContol(self, *args):
-        self.listbox_names.yview(*args)
-        self.listbox_wins.yview(*args)
-        self.listbox_losses.yview(*args)
-        self.listbox_ratios.yview(*args)
+    def TreeviewSetup(self, canvas):
+        # data columns
+        headings = ('name', 'wins', 'losses', 'ratio')
+
+        # Treeview setup
+        tree = ttk.Treeview(columns=headings)
+
+        # headings and columns setup
+        tree.heading('#0', text='#', anchor=tk.E)
+        tree.heading('name', text='Name', anchor=tk.W)
+        tree.heading('wins', text='Wins', anchor=tk.E)
+        tree.heading('losses', text='Losses', anchor=tk.E)
+        tree.heading('ratio', text='W/L Ratio', anchor=tk.E)
+
+        tree.column('#0', stretch=0, width=50, anchor=tk.E)
+        tree.column('name', stretch=1, width=100, anchor=tk.W)
+        tree.column('wins', stretch=0, width=50, anchor=tk.E)
+        tree.column('losses', stretch=0, width=50, anchor=tk.E)
+        tree.column('ratio', stretch=0, width=75, anchor=tk.E)
+
+        # scrollbar setup
+        y_scroll_bar = ttk.Scrollbar(orient=tk.VERTICAL, command=tree.yview)
+        x_scroll_bar = ttk.Scrollbar(orient=tk.HORIZONTAL, command=tree.xview)
+        tree['yscroll'] = y_scroll_bar.set
+        tree['xscroll'] = x_scroll_bar.set
+
+        # grid setup
+        tree.grid(in_=canvas, row=1, column=0, sticky=tk.NSEW)
+        y_scroll_bar.grid(in_=canvas, row=1, column=1, sticky=tk.NS)
+        x_scroll_bar.grid(in_=canvas, row=2, column=0, sticky=tk.EW)
+
+        # resize the column size of the window
+        canvas.columnconfigure(0, weight=1)
+
+        # FIXME:
+        ttk.Style().configure("Treeview", fieldbackground="red")
+
+        # tags for even and odd to show rows
+        tree.tag_configure('even', background='pink')
+        tree.tag_configure('odd', background='lightgreen')
+
+        return tree
+
+    
+    def fillTreeview(self, tree):
+        for i, player in enumerate(self.playerlist, start=1):
+            data = [player.name, player.wins, player.losses, player.ratio]
+            tree.insert('', tk.END, text='%3d'%i, values=data, tags=('even'))
